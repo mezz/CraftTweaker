25a4b1f8afe8008fd2437f5e4d82df44a0d13529
Jared <jaredlll08@gmail.com>
2017-01-03 01:10:49 +0200

    Added some debug to the dummy jei

--- MineTweaker3-API/src/main/java/minetweaker/api/compat/DummyJEIRecipeRegistry.java
+++ MineTweaker3-API/src/main/java/minetweaker/api/compat/DummyJEIRecipeRegistry.java
@@ -6,19 +6,21 @@ public class DummyJEIRecipeRegistry implements IJEIRecipeRegistry {
 	
 	@Override
 	public void addRecipe(Object object) {
+		System.out.println("Dummy cannot add recipe!");
 	}
 	
 	@Override
 	public void removeRecipe(Object object) {
+		System.out.println("Dummy cannot remove recipe!");
 	}
 	
 	@Override
 	public void addFurnace(List<Object> inputs, Object output) {
-		
+		System.out.println("Dummy cannot add recipe!");
 	}
 	
 	@Override
 	public void removeFurnace(Object object) {
-		
+		System.out.println("Dummy cannot remove recipe!");
 	}
 }
